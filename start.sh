5928cabb63f67ad547a0bc75b238531ff492a165
EduGM - Wanetty <eduard.gm@gmail.com>
2021-11-11 19:57:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
+install_ssl_Cert() {
+	git clone https://github.com/certbot/certbot.git /opt/letsencrypt > /dev/null 2>&1
+	command="/opt/letsencrypt/certbot-auto certonly --standalone $DOMAIN -n --register-unsafely-without-email --agree-tos"
+	eval $command
+
+}
+
+
+
 mkdir -p /etc/opendkim/keys/$DOMAIN/
 opendkim-genkey -s 202109 -d $DOMAIN -D /etc/opendkim/keys/$DOMAIN/
 chown -R opendkim:opendkim /etc/opendkim/keys/$DOMAIN/
@@ -17,8 +26,10 @@ sed -i "s/_DOMAIN_/$DOMAIN/g" /etc/opendkim.conf
 sed -i "s/_DOMAIN_/$DOMAIN/g" /etc/postfix/main.cf
 sed -i "s/ _PUBLIC_IP_/$PUBLIC_IP/g" /etc/postfix/main.cf
 
+
 service postfix start
 postfix reload
 service opendkim start
+install_ssl_Cert
 cd gophish
 ./gophish
